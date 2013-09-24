660db789b1011739fa86002a8e304aabd4ddda3f
Scott Duckworth <sduckwo@clemson.edu>
2013-09-24 16:21:22 -0400

    add files for python setuptools

--- anyvcs/__init__.py
+++ anyvcs/__init__.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with python-anyvcs.  If not, see <http://www.gnu.org/licenses/>.
 
+__version__ = '0.0.0'
 
 def create(path, vcs):
   from common import UnknownVCSType
---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+execfile('anyvcs/__init__.py')
+
+setup(
+  name='python-anyvcs',
+  version=__version__,
+  packages=['anyvcs'],
+  include_package_data=True,
+  license='GNU Lesser General Public License v3 (LGPLv3)',
+  description='An abstraction layer for multiple version control systems.',
+  long_description=README,
+  url='https://github.com/ScottDuckworth/python-anyvcs',
+  author='Scott Duckworth',
+  author_email='sduckwo@clemson.edu',
+  classifiers=[
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Operating System :: POSIX',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Version Control',
+  ],
+)
