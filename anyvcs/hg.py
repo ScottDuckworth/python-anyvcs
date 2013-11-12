00172a62c69b74b29c558ace2add3d89b4420e88
Scott Duckworth <sduckwo@clemson.edu>
2013-11-12 17:18:00 -0500

    start adding support for python 2.5

--- anyvcs/hg.py
+++ anyvcs/hg.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with python-anyvcs.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import with_statement
 import datetime
 import os
 import re
