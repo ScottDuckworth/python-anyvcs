d5274c3d6a224603100c402a92c61d5ac4243095
Scott Duckworth <sduckwo@clemson.edu>
2013-11-13 09:24:05 -0500

    bump version to 1.2.1

--- anyvcs/__init__.py
+++ anyvcs/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with python-anyvcs.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 def create(path, vcs):
   """Create a new repository
