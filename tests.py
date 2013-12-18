43ab35308fc897f11037a5fe6027674551a0bdbd
Scott Duckworth <sduckwo@clemson.edu>
2013-12-18 16:20:39 -0500

    use sys.hexversion for python version checks

--- tests.py
+++ tests.py
@@ -36,7 +36,7 @@ import subprocess
 import sys
 import tempfile
 import time
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+if sys.hexversion < 0x02070000:
   import unittest2 as unittest
 else:
   import unittest
