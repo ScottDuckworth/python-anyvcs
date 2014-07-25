3aff2fe153788d2df263ee336cb11dd4fbe65d5f
Paul Kilgo <pkilgo@clemson.edu>
2014-07-25 14:49:28 -0400

    add test methods for proplist

--- tests.py
+++ tests.py
@@ -1185,6 +1185,17 @@ class SvnBasicTest(SvnTest, BasicTest):
         self.assertIsInstance(result[0].date, datetime.datetime)
         self.assertEqual('Pisgah'.encode(), result[0].line)
 
+    def test_proplist(self):
+        result = self.repo.proplist(self.rev1)
+        expected = sorted(['svn:log', 'svn:author', 'svn:date'])
+        self.assertEqual(expected, sorted(result))
+
+    def test_proplist_path(self):
+        result = self.repo.proplist(self.rev1, 'a')
+        expected = []
+        self.assertEqual(expected, result)
+
+
 ### TEST CASE: UnrelatedBranchTest ###
 
 
